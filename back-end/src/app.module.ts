c6ea3533848d9a9f37499de19381542241fac94e
ByunMooYoung <mk@mklc.co.kr>
2021-11-10 10:46:21 +0900

    a

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -6,6 +6,12 @@ import { AuthModule } from './lib/auth/auth.module';
 import { CommonModule } from './lib/common/common.module';
 import configuration from './config/configuration';
 
+const dbInfo = (process.env.DB_USE_AUTH)
+  ? 'mongodb://' + process.env.DB_ID + ":" + process.env.DB_PWD + "@"
+  + process.env.DB_HOST + ":" + process.env.DB_PORT + '/' + process.env.DB_NAME
+  : 'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME;
+console.log(dbInfo);
+
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -13,10 +19,12 @@ import configuration from './config/configuration';
       cache: true,
       load: [configuration],
     }),
-    TypegooseModule.forRoot(
-      'mongodb://' + process.env.DB_HOST + '/' + process.env.DB_NAME,
-    ),
-    // TypegooseModule.forRoot('mongodb://test:test1234@54.178.58.215:9003/admin'),
+    // TypegooseModule.forRoot('mongodb://mk:mk1234@3.36.252.198:9003/admin'),
+    // TypegooseModule.forRoot(
+    //   'mongodb://' + process.env.DB_ID + ":" + process.env.DB_PWD + "@"
+    //   + process.env.DB_HOST + '/' + process.env.DB_NAME
+    // ),
+    TypegooseModule.forRoot(dbInfo),
     ModulesModule,
     AuthModule,
     CommonModule,
