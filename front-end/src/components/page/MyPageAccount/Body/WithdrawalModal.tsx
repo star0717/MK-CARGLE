a407dc33ef50fc236133dba5ef11f6e706b7a205
jeong gu hyoek <rngur1205@naver.com>
2021-12-01 07:43:46 +0000

    a

--- front-end/src/components/page/MyPageAccount/Body/WithdrawalModal.tsx
+++ front-end/src/components/page/MyPageAccount/Body/WithdrawalModal.tsx
@@ -7,7 +7,7 @@ const WithdrawalModal: NextPage = () => {
       <Wrapper>
         <Text>모든 데이터가 삭제됩니다.</Text>
         <Text>정말로 탈퇴 하시겠습니까?</Text>
-        <Wrapper>
+        <Wrapper dr={`row`}>
           <button>회원 탈퇴</button>
           <button>취소</button>
         </Wrapper>
