90c6ad76582239416974b5e92a395c595cefb1b5
ByunMooYoung <mk@mklc.co.kr>
2021-12-02 16:13:12 +0900

    a

--- front-end/src/models/company.entity.ts
+++ front-end/src/models/company.entity.ts
@@ -1,19 +1,19 @@
 import { BaseEntity } from "./base.entity";
 export enum CompanyApproval {
-    BEFORE = "before",
-    ING = "ing",
-    DONE = "done"
+  BEFORE = "before",
+  ING = "ing",
+  DONE = "done",
 }
 export declare class Company extends BaseEntity {
-    name: string;
-    comRegNum: string;
-    mbRegNum: string;
-    mbTypeNum: string;
-    ownerName: string;
-    busType: string;
-    busItem: string;
-    phoneNum: string;
-    faxNum: string;
-    address: string;
-    approval: CompanyApproval;
+  name: string;
+  comRegNum: string;
+  mbRegNum: string;
+  mbTypeNum: string;
+  ownerName: string;
+  busType: string;
+  busItem: string;
+  phoneNum: string;
+  faxNum: string;
+  address: string;
+  approval: CompanyApproval;
 }
