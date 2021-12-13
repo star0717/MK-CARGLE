8fc0bfea817b8447961eabb797dec3fd8009cbfb
Ubuntu <ubuntu@ip-172-26-6-54.ap-southeast-1.compute.internal>
2021-12-13 06:59:15 +0000

    a

--- front-end/src/components/page/Find/body/findEmailComPresenter.tsx
+++ front-end/src/components/page/Find/body/findEmailComPresenter.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
 import {
   CommonButton,
+  CommonButtonWrapper,
   Text,
   WholeWrapper,
   Wrapper,
@@ -15,17 +16,42 @@ const FindEmailComplete: NextPage<any> = (props) => {
 
   return (
     <WholeWrapper>
-      <Text>회원님의 이메일은 다음과 같습니다.</Text>
-      <Text>{findEmail}</Text>
+      <Text padding={`0px 0px 30px`} fontSize={`22px`}>
+        회원님의 이메일은 다음과 같습니다.
+      </Text>
+      <Wrapper
+        width={`500px`}
+        height={`100px`}
+        border={`1px solid #ccc`}
+        radius={`5px`}
+        margin={`0px 0px 30px`}
+      >
+        <Text
+          fontSize={`22px`}
+          fontWeight={`600`}
+          color={`#0066ff`}
+        >{findEmail}</Text>
+      </Wrapper>
       <Wrapper>
-        <CommonButton
-          type="button"
-          onClick={() => {
-            router.push("/");
-          }}
-        >
-          로그인
-        </CommonButton>
+        <CommonButtonWrapper>
+          <CommonButton
+            type="button"
+            onClick={() => {
+              router.push("/");
+            }}
+            kindOf={`white`}
+            margin={`0px 0px 20px`}
+          >
+            로그인
+          </CommonButton>
+          <CommonButton type="button"
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+              router.push('/sign/findpassword')
+            }}
+          >
+            비밀번호 찾기
+          </CommonButton>
+        </CommonButtonWrapper>
       </Wrapper>
     </WholeWrapper>
   );
