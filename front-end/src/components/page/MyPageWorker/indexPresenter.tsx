491852fd0c531fbb113b8c8783b51a4c1e05f56a
ByunMooYoung <mk@mklc.co.kr>
2021-12-20 16:10:35 +0900

    a

--- front-end/src/components/page/MyPageWorker/indexPresenter.tsx
+++ front-end/src/components/page/MyPageWorker/indexPresenter.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import workerInfo from "./section/workerInfo";
 
 const MyPageWorkerPresenter: NextPage<any> = (props) => {
-  return <WholeWrapper>{<workerInfo {...props} />}</WholeWrapper>;
+  return <WholeWrapper>{/* <workerInfo {...props} /> */}</WholeWrapper>;
 };
 
 export default MyPageWorkerPresenter;
