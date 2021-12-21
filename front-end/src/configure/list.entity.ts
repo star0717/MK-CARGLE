8905c47235a1d826d080be8843b791e59c968544
ByunMooYoung <mk@mklc.co.kr>
2021-12-21 17:58:33 +0900

    a

--- front-end/src/configure/list.entity.ts
+++ front-end/src/configure/list.entity.ts
@@ -1,3 +1,4 @@
+import { UserAuthority } from "../models/user.entity";
 import { MbType } from "./etc.entity";
 import { UseLink } from "./router.entity";
 
@@ -18,21 +19,25 @@ export const menuList = [
     key: "1",
     menuName: "부품",
     link: "#",
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "1.1",
         subMenuName: "부품 관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "1.2",
         subMenuName: "세트 관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "1.3",
         subMenuName: "거래처 관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
     ],
   },
@@ -40,21 +45,25 @@ export const menuList = [
     key: "2",
     menuName: "정비",
     link: "#",
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "2.1",
         subMenuName: "정비장부",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "2.2",
         subMenuName: "고객/차량 관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "2.3",
         subMenuName: "예약관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
     ],
   },
@@ -62,16 +71,19 @@ export const menuList = [
     key: "3",
     menuName: "회계",
     link: "#",
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "3.1",
         subMenuName: "매입/매출 현황",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "3.2",
         subMenuName: "결산 리포트",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
     ],
   },
@@ -79,16 +91,19 @@ export const menuList = [
     key: "4",
     menuName: "커뮤니티",
     link: "#",
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "4.1",
         subMenuName: "연합회 소식",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "4.2",
         subMenuName: "정비정보 공유",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
     ],
   },
@@ -96,21 +111,25 @@ export const menuList = [
     key: "5",
     menuName: "마이페이지",
     link: UseLink.MYPAGE_ACCOUNT,
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "5.1",
         subMenuName: "계정 관리",
         subMenuLink: UseLink.MYPAGE_ACCOUNT,
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "5.2",
         subMenuName: "직원 관리",
         subMenuLink: UseLink.MYPAGE_WORKER,
+        subMenuAuth: UserAuthority.OWNER,
       },
       {
         key: "5.3",
         subMenuName: "포인트 관리",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
     ],
   },
@@ -118,16 +137,39 @@ export const menuList = [
     key: "6",
     menuName: "고객센터",
     link: "#",
+    auth: UserAuthority.WORKER,
     subMenu: [
       {
         key: "6.1",
         subMenuName: "공지사항",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
       },
       {
         key: "6.2",
         subMenuName: "사용 설명서",
         subMenuLink: "#",
+        subMenuAuth: UserAuthority.WORKER,
+      },
+    ],
+  },
+  {
+    key: "7",
+    menuName: "관리자",
+    link: "#",
+    auth: UserAuthority.ADMIN,
+    subMenu: [
+      {
+        key: "7.1",
+        subMenuName: "승인관리",
+        subMenuLink: "#",
+        subMenuAuth: UserAuthority.ADMIN,
+      },
+      {
+        key: "7.2",
+        subMenuName: "???",
+        subMenuLink: "#",
+        subMenuAuth: UserAuthority.ADMIN,
       },
     ],
   },
