946c53b1c2c3602cfa6f11944ad489e0bad73cfb
ByunMooYoung <mk@mklc.co.kr>
2021-12-23 13:51:51 +0900

    a

--- front-end/src/components/page/MyPageWorker/section/workerInfoModal.tsx
+++ front-end/src/components/page/MyPageWorker/section/workerInfoModal.tsx
@@ -1,7 +1,14 @@
 import { Switch } from "@material-ui/core";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+
+dayjs.locale("ko");
 import type { NextPage } from "next";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { _cWorkerInfoModalProps } from "../../../../configure/_cProps.entity";
+import { SignUpInfo } from "../../../../models/auth.entity";
+import { User } from "../../../../models/user.entity";
 import {
   dateToString,
   makeFullAddress,
@@ -16,9 +23,14 @@ import {
 } from "../../../styles/CommonComponents";
 
 const WorkerInfoModal: NextPage<_cWorkerInfoModalProps> = (props) => {
-  console.log(props.clickDoc);
+  const dispatch = useDispatch();
   // state 관리
   const [approval, setApproval] = useState<boolean>(props.clickDoc.approval); // 직원 승인여부
+  const [docInfo, setDocInfo] = useState<User>(props.clickDoc);
+
+  const onChangeWorkerInfo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <WholeWrapper>
@@ -32,59 +44,59 @@ const WorkerInfoModal: NextPage<_cWorkerInfoModalProps> = (props) => {
             onChange={() => setApproval(!approval)}
           />
         </Wrapper>
-        <Wrapper dr={`row`}>
-          <Text>직원명</Text>
-          <TextInput2 type="text" />
-        </Wrapper>
-        <Wrapper dr={`row`}>
-          <Text>아이디</Text>
-          <TextInput2 type="text" />
-        </Wrapper>
-        <Wrapper dr={`row`}>
-          <Text>휴대폰번호</Text>
-          <TextInput2 type="text" />
-        </Wrapper>
-        <Wrapper dr={`row`}>
-          <Text>주소</Text>
-          <TextInput2
-            value={makeFullAddress(
-              props.clickDoc.address1,
-              props.clickDoc.address2,
-              props.clickDoc.postcode
-            )}
-            type="text"
-            readOnly
-          />
-        </Wrapper>
-        <Wrapper dr={`row`}>
-          <Text>입사일자</Text>
-          <TextInput2
-            type="date"
-            name="joinDate"
-            // value={
-            //   props.clickDoc.joinDate
-            //     ? props.clickDoc.joinDate.toString().slice(0, 10)
-            //     : ""
-            // }
-            value={dateToString(props.clickDoc.joinDate)}
-          />
-        </Wrapper>
-        <Wrapper>
-          <SmallButton
-            type="button"
-            kindOf={`default`}
-            margin={`0px 0px 0px 20px`}
-          >
-            저장
-          </SmallButton>
-          <SmallButton
-            type="button"
-            kindOf={`default`}
-            margin={`0px 0px 0px 20px`}
-          >
-            직원삭제
-          </SmallButton>
-        </Wrapper>
+        <form onSubmit={onChangeWorkerInfo}>
+          <Wrapper dr={`row`}>
+            <Text>직원명</Text>
+            <TextInput2 type="text" value={docInfo.name} readOnly />
+          </Wrapper>
+          <Wrapper dr={`row`}>
+            <Text>아이디</Text>
+            <TextInput2 type="text" value={docInfo.email} readOnly />
+          </Wrapper>
+          <Wrapper dr={`row`}>
+            <Text>휴대폰번호</Text>
+            <TextInput2 type="text" value={docInfo.hpNumber} readOnly />
+          </Wrapper>
+          <Wrapper dr={`row`}>
+            <Text>주소</Text>
+            <TextInput2
+              value={makeFullAddress(
+                docInfo.address1,
+                docInfo.address2,
+                docInfo.postcode
+              )}
+              type="text"
+              readOnly
+            />
+          </Wrapper>
+          <Wrapper dr={`row`}>
+            <Text>입사일자</Text>
+            <TextInput2
+              type="date"
+              name="joinDate"
+              value={dayjs(docInfo.joinDate).format("YYYY-MM-DD")}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setDocInfo({ ...docInfo, joinDate: new Date(e.target.value) });
+              }}
+            />
+          </Wrapper>
+          <Wrapper>
+            <SmallButton
+              type="submit"
+              kindOf={`default`}
+              margin={`0px 0px 0px 20px`}
+            >
+              저장
+            </SmallButton>
+            <SmallButton
+              type="button"
+              kindOf={`default`}
+              margin={`0px 0px 0px 20px`}
+            >
+              직원삭제
+            </SmallButton>
+          </Wrapper>
+        </form>
       </RsWrapper>
     </WholeWrapper>
   );
