fd758b3e2e2b26ae00cfd17c0b1c2f9c6c244c66
ByunMooYoung <mk@mklc.co.kr>
2021-12-23 17:59:43 +0900

    a

--- front-end/pages/byeolTest.tsx
+++ front-end/pages/byeolTest.tsx
@@ -9,6 +9,10 @@ import {
   RsWrapper,
   Test,
   FooterWrapper,
+  TextInput2,
+  CommonTitle,
+  CommonSubTitle,
+  CommonTitleWrapper,
 } from "../../../n2server/front-end/src/components/styles/CommonComponents";
 import Header from "../src/components/layout/Header";
 import Footer from "../src/components/layout/Footer";
@@ -19,14 +23,8 @@ import Footer from "../src/components/layout/Footer";
  */
 const ByeolTest: NextPage = () => {
   return (
-    <div
-      style={{
-        height: `100%`,
-        // position: `relative`,
-        backgroundColor: `#000`,
-      }}
-    >
-      <div style={{ minHeight: `100%`, backgroundColor: `#000` }}>
+    <WholeWrapper>
+      {/* <div style={{ minHeight: `100%`, backgroundColor: `#000` }}>
         <div
           style={{
             width: `100%`,
@@ -36,9 +34,29 @@ const ByeolTest: NextPage = () => {
         >
           컨텐츠
         </div>
-      </div>
+      </div> */}
+      <CommonTitleWrapper>
+        <CommonTitle>계정관리</CommonTitle>
+        <CommonSubTitle>
+          계정관리를 위해 비밀번호를 입력해주세요.
+        </CommonSubTitle>
+      </CommonTitleWrapper>
+      <RsWrapper>
+        <Wrapper width={`500px`}>
+          <form>
+            <TextInput2
+              placeholder="비밀번호를 입력하세요."
+              width={`500px`}
+              height={`60px`}
+              margin={`0px 0px 10px 0px`}
+            />
+
+            <CommonButton type="submit">확인</CommonButton>
+          </form>
+        </Wrapper>
+      </RsWrapper>
       <FooterWrapper bgColor={`#ddd`}>I'm Footer</FooterWrapper>
-    </div>
+    </WholeWrapper>
   );
 };
 
