764d5530259fec752c03235b476eaaa2cd499fd5
ByunMooYoung <mk@mklc.co.kr>
2021-12-24 11:35:54 +0900

    a

--- front-end/src/components/page/SignIn/SignIn.tsx
+++ front-end/src/components/page/SignIn/SignIn.tsx
@@ -1,17 +1,31 @@
 import type { NextPage } from "next";
+import React, { useState } from "react";
+import Link from "next/link";
 import Cookies from "js-cookie";
 import { useRouter } from "next/dist/client/router";
-import React, { useState } from "react";
+import { useResizeDetector } from "react-resize-detector";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 import { useDispatch, useSelector } from "react-redux";
 import { actionTypesUser, UserState } from "../../../../store/interfaces";
 import { RootStateInterface } from "../../../../store/interfaces/RootState";
 import { signInUserAction } from "../../../../store/action/user.action";
 import { formRegEx } from "../../../validation/regEx";
-import SigninPresenter from "./SignInPresenter";
 import { UserInfo } from "../../../models/auth.entity";
 import { UseLink } from "../../../configure/router.entity";
 import { _SignInProps } from "../../../configure/_props.entity";
-import { _pSignInProps } from "../../../configure/_pProps.entity";
+import {
+  WholeWrapper,
+  RsWrapper,
+  Wrapper,
+  CommonTitle,
+  CommonButton,
+  TextInput2,
+  Text,
+  CommonSubTitle,
+  Label,
+} from "../../styles/CommonComponents";
 
 /**
  * 로그인 컴포넌트(기능)
@@ -90,17 +104,147 @@ const SignIn: NextPage<_SignInProps> = (props) => {
     dispatch({ type: actionTypesUser.USER_INIT });
   };
 
-  // 화면 구성에 넘겨줄 props
-  const fProps: _pSignInProps = {
-    onSignInHandler,
-    inputSignIn,
-    onInputHandler,
-    saveCheck,
-    setSaveCheck,
-    userInit,
-  };
+  // resize 변수 선언
+  const { width, height, ref } = useResizeDetector();
 
-  return <SigninPresenter {...fProps} />;
+  return (
+    <WholeWrapper ref={ref}>
+      <RsWrapper>
+        <Wrapper
+          width={width < 1439 ? (width < 500 ? `300px` : `400px`) : `500px`}
+        >
+          <CommonTitle textAlign={`center`} margin={`0px`} padding={`0px`}>
+            CARGLE
+          </CommonTitle>
+          <CommonSubTitle color={`#000`}>
+            정비관리도 이젠 편리하게
+          </CommonSubTitle>
+
+          <Wrapper margin={`20px 0px 0px`}>
+            {/* 로그인 입력 form */}
+            <Wrapper
+              width={width < 1439 ? (width < 500 ? `300px` : `400px`) : `500px`}
+            >
+              <form onSubmit={onSignInHandler}>
+                <TextInput2
+                  marginBottom={`10px`}
+                  width={
+                    width < 1439 ? (width < 500 ? `300px` : `400px`) : `500px`
+                  }
+                  height={`60px`}
+                  placeholder="이메일"
+                  type="text"
+                  name="id"
+                  value={inputSignIn.id}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onInputHandler(e);
+                  }}
+                />
+                <Wrapper>
+                  <Text
+                    fontSize={width < 500 ? `12px` : `16px`}
+                    fontWeight={`700`}
+                    margin={`5px 0px 10px 0px`}
+                    width={`100%`}
+                    display={`flex`}
+                    ju={`flex-start`}
+                  >
+                    TIP! 아이디는 가입 시 입력한 이메일을 입력해주세요.
+                  </Text>
+                </Wrapper>
+                {/* 비밀번호 input */}
+
+                <TextInput2
+                  width={
+                    width < 1439 ? (width < 500 ? `300px` : `400px`) : `500px`
+                  }
+                  height={`60px`}
+                  marginTop={`10px`}
+                  type="password"
+                  placeholder="비밀번호"
+                  name="pwd"
+                  value={inputSignIn.pwd}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    onInputHandler(e);
+                  }}
+                />
+
+                {/* 체크박스 div */}
+
+                <Wrapper dr={`row`} ju={`flex-end`}>
+                  <Label width={`100%`} margin={`4px 0`} ju={`flex-end`}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={saveCheck}
+                          onChange={(
+                            e: React.ChangeEvent<HTMLInputElement>
+                          ) => {
+                            setSaveCheck(e.target.checked);
+                          }}
+                        />
+                      }
+                      label="아이디 저장"
+                    />
+                  </Label>
+                </Wrapper>
+                {/* 로그인 버튼 */}
+                <CommonButton
+                  type="submit"
+                  width={
+                    width < 1439 ? (width < 500 ? `300px` : `400px`) : `500px`
+                  }
+                >
+                  로그인
+                </CommonButton>
+              </form>
+            </Wrapper>
+            <Wrapper padding={`10px 0px`}>
+              <Wrapper
+                dr={`row`}
+                fontSize={width < 500 ? `12px` : `16px`}
+                ju={`flex-end`}
+              >
+                <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  style={{ width: "18px", margin: "0px 3px 4px 0px" }}
+                />
+                <Text margin={`0px 3px 0px`} fontSize={`16px`}>
+                  아직 회원이 아니신가요?
+                </Text>
+                <Text decoration={`underline`}>
+                  <Link href={UseLink.SIGNUP}>
+                    <a
+                      style={{ fontWeight: "bold", fontSize: "16px" }}
+                      onClick={userInit}
+                    >
+                      회원가입
+                    </a>
+                  </Link>
+                </Text>
+              </Wrapper>
+              <Wrapper dr={`row`} fontSize={`16px`} ju={`flex-end`}>
+                <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  style={{ width: "18px", margin: "0px 3px 4px 0px" }}
+                />
+                <Text margin={`0px 3px 0px`} fontSize={`16px`}>
+                  계정을 찾고 싶으신가요?
+                </Text>
+                <Text decoration={`underline`}>
+                  <Link href={UseLink.FIND_EMAIL}>
+                    <a style={{ fontWeight: "bold", fontSize: "16px" }}>
+                      계정찾기
+                    </a>
+                  </Link>
+                </Text>
+              </Wrapper>
+            </Wrapper>
+          </Wrapper>
+        </Wrapper>
+      </RsWrapper>
+    </WholeWrapper>
+  );
 };
 
 export default SignIn;
