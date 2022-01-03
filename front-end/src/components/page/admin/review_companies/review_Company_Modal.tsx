2328f139f538002a11dfbd1740bbabf7597fa52a
ByunMooYoung <mk@mklc.co.kr>
2022-01-03 15:43:35 +0900

    a

--- front-end/src/components/page/admin/review_companies/review_Company_Modal.tsx
+++ front-end/src/components/page/admin/review_companies/review_Company_Modal.tsx
@@ -97,44 +97,6 @@ const AdminReviewCompaniesModal: NextPage<any> = (props) => {
               </Wrapper>
             </Wrapper>
             </RsWrapper>
-          <Wrapper>
-            <Modal
-              isOpen={modalOpen}
-              style={{
-                overlay: {
-                  position: "fixed",
-                  zIndex: 1020,
-                  top: 0,
-                  left: 0,
-                  width: "100vw",
-                  height: "100vh",
-                  background: "rgba(255, 255, 255, 0.75)",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                },
-                content: {
-                  background: "white",
-                  width: "45rem",
-                  height: "575px",
-                  maxWidth: "calc(100vw - 2rem)",
-                  maxHeight: "calc(100vh - 2rem)",
-                  overflowY: "auto",
-                  position: "relative",
-                  border: "1px solid #ccc",
-                  borderRadius: "0.3rem",
-                  boxShadow: "0px 10px 15px rgba(220,220,220,1)",
-                  inset: 0,
-                },
-              }}
-            >
-              <Wrapper fontSize={`28px`} al={`flex-end`}>
-                <CloseButton onClick={closeModal}>
-                  <IoIosCloseCircle />
-                </CloseButton>
-              </Wrapper>
-            </Modal>
-            </Wrapper>
           </WholeWrapper>;
 };
 
