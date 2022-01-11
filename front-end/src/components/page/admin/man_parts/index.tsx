6c68d399e95f5f476b8ca26dbf7ba2f475a82f9b
ByunMooYoung <ydb0717@gmail.com>
2022-01-11 14:27:55 +0900

    a

--- front-end/src/components/page/admin/man_parts/index.tsx
+++ front-end/src/components/page/admin/man_parts/index.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from "next";
+import Modal from "react-modal";
 import { useResizeDetector } from "react-resize-detector";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BodyWrapper } from "../../../styles/LayoutComponents";
 import {
+  CommonTitle,
+  CommonTitleWrapper,
   IconButton,
   RsWrapper,
   SearchInput,
@@ -10,13 +13,18 @@ import {
   TableHead,
   TableHeadLIST,
   TableRow,
+  SmallButton,
   TableRowLIST,
+  CloseButton,
   TableWrapper,
   Text,
   WholeWrapper,
   Wrapper,
+  CommonButton,
 } from "../../../styles/CommonComponents";
-import { BsSearch } from "react-icons/bs";
+import { BsPlusSquareFill, BsSearch } from "react-icons/bs";
+import { AiFillMinusSquare, AiFillPlusSquare } from "react-icons/ai";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const AdminManPartsPage: NextPage<any> = (props) => {
   /*********************************************************************
@@ -26,6 +34,7 @@ const AdminManPartsPage: NextPage<any> = (props) => {
   /*********************************************************************
    * 2. State settings
    *********************************************************************/
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [partClass, setPartClass] = useState<any>();
   const [allPart, setAllPart] = useState<any>();
   /*********************************************************************
@@ -35,7 +44,17 @@ const AdminManPartsPage: NextPage<any> = (props) => {
   /*********************************************************************
    * 4. Props settings
    *********************************************************************/
+  const closeModal = () => {
+    setModalOpen(false);
+    props.findDocHandler(props.findResult.currentPage);
+  };
 
+  // modal 창 팝업 시 뒤에 배경 scroll 막기
+  useEffect(() => {
+    modalOpen === true
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "unset");
+  }, [modalOpen]);
   /*********************************************************************
    * 5. Page configuration
    *********************************************************************/
@@ -46,7 +65,115 @@ const AdminManPartsPage: NextPage<any> = (props) => {
   return (
     <BodyWrapper ref={ref}>
       <WholeWrapper>
-        <RsWrapper></RsWrapper>
+        <CommonTitleWrapper>
+          <CommonTitle>부품관리</CommonTitle>
+        </CommonTitleWrapper>
+        <CommonButton>hyeok modal</CommonButton>
+        <RsWrapper>
+          <Wrapper dr={`row`} padding={`40px 0px 0px`} ju={`space-between`}>
+            {/* 부품분류 */}
+            <Wrapper
+              width={`300px`}
+              height={`600px`}
+              ju={`flex-start`}
+              bgColor={`#fff`}
+              overflow={`auto`}
+              radius={`5px 5px 0px 0px`}
+              border={`1px solid #c4c4c4`}
+            >
+              <Wrapper
+                bgColor={`#343a40`}
+                color={`#fff`}
+                height={`45px`}
+                radius={`5px 5px 0px 0px`}
+              >
+                <Text>부품분류</Text>
+              </Wrapper>
+              <Wrapper padding={`10px`}>
+                <Wrapper
+                  height={`45px`}
+                  bgColor={`#fff`}
+                  border={`1px solid #c4c4c4`}
+                  dr={`row`}
+                  radius={`5px`}
+                  padding={`0px 20px`}
+                  margin={`0px 0px 10px`}
+                >
+                  <Wrapper width={`22px`} color={`#51b351`} fontSize={`22px`}>
+                    <AiFillPlusSquare />
+                  </Wrapper>
+                  <Wrapper>
+                    <Text width={`200px`}>전체보기</Text>
+                  </Wrapper>
+                </Wrapper>
+
+                {/* focus */}
+                <Wrapper
+                  height={`45px`}
+                  bgColor={`#eee`}
+                  border={`1px solid #c4c4c4`}
+                  dr={`row`}
+                  radius={`5px`}
+                  padding={`0px 20px`}
+                  margin={`0px 0px 10px`}
+                >
+                  <Wrapper width={`22px`} fontSize={`22px`} color={`#d6263b`}>
+                    <AiFillMinusSquare />
+                  </Wrapper>
+                  <Wrapper>
+                    <Text width={`200px`}>부품명(01)</Text>
+                  </Wrapper>
+                </Wrapper>
+                {/* focus */}
+              </Wrapper>
+            </Wrapper>
+
+            {/* 상세정보 */}
+            <Wrapper bgColor={`#ccc`} width={`880px`}>
+              deklkfgl;skf
+            </Wrapper>
+            {/* 상세정보 */}
+          </Wrapper>
+        </RsWrapper>
+        <Wrapper>
+          <Modal
+            isOpen={modalOpen}
+            style={{
+              overlay: {
+                position: "fixed",
+                zIndex: 1020,
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                background: "rgba(71, 71, 71, 0.75)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              },
+              content: {
+                background: "white",
+                width: "500px",
+                height: "800px",
+                maxWidth: "calc(100vw - 2rem)",
+                maxHeight: "calc(100vh - 2rem)",
+                overflowY: "auto",
+                position: "relative",
+                border: "1px solid #ccc",
+                borderRadius: "0.3rem",
+                boxShadow: "0px 10px 15px rgba(61,61,61,1)",
+                inset: 0,
+              },
+            }}
+          >
+            <Wrapper fontSize={`28px`} al={`flex-end`}>
+              <CloseButton onClick={closeModal}>
+                <IoIosCloseCircle />
+              </CloseButton>
+              <parts_Modal />
+            </Wrapper>
+          </Modal>
+        </Wrapper>
       </WholeWrapper>
     </BodyWrapper>
   );
