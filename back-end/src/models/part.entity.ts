c93694254f011a87f72bcdf615dfdcdca63fcbfd
ByunMooYoung <mk@mklc.co.kr>
2022-01-13 17:55:30 +0900

    a

--- back-end/src/models/part.entity.ts
+++ back-end/src/models/part.entity.ts
@@ -8,3 +8,20 @@ export class PartCategory {
   code: string;
   description?: string;
 }
+
+export class TS_Code {
+  // 분류
+  class: string;
+  // 인덱스
+  index: string;
+  // 코드
+  code: string;
+  // 이름
+  name: string;
+  // 별칭
+  nickName?: string;
+  // 옵션
+  options?: [string];
+}
+
+export const TS_CodeList: [TS_Code] = [];
