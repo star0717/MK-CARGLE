3b7ea8de21bc109f42e04ef4b3407ec51674553d
ByunMooYoung <hbc3869@gmail.com>
2022-01-20 13:50:41 +0900

    a

--- front-end/src/components/page/admin/molit_items/index.tsx
+++ front-end/src/components/page/admin/molit_items/index.tsx
@@ -1,9 +1,7 @@
 import { NextPage } from "next";
 import { useResizeDetector } from "react-resize-detector";
-
 import React, { useEffect, useState } from "react";
 import { BodyWrapper } from "../../../styles/LayoutComponents";
-import { AiFillPlusSquare, AiFillMinusSquare } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import {
   WholeWrapper,
@@ -22,7 +20,6 @@ import {
   TableRowLIST,
   Text,
 } from "../../../styles/CommonComponents";
-import { MdOutlineNavigateNext } from "react-icons/md";
 import {
   TsItem,
   TsClass,
@@ -43,39 +40,19 @@ const AdminMolitItemsPage: NextPage<any> = (props) => {
   /*********************************************************************
    * 2. State settings
    *********************************************************************/
-  const [modalOpen, setModalOpen] = useState<boolean>(false); // modal 창 여부
-  const [modalOption, setModalOption] = useState<string>(""); // modal 옵션
   const [searchText, setSearchText] = useState<string>(""); // 검색 텍스트
   const [selectClass, setSelectClass] = useState<string>("all"); // 선택한 분류
   const [tsItemList, setTsItemList] = useState<TsItem[]>(tsItemListAll); // 선택한 국토부 리스트
-  const [clickDoc, setClickDoc] = useState<TsItem>(); // 선택한 부품 항목 데이터
+  const [reset, setReset] = useState<number>(0); // 리스트 재출력 여부
+
   /*********************************************************************
    * 3. Handlers
    *********************************************************************/
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
-  // modal 창 팝업 시 뒤에 배경 scroll 막기
-  useEffect(() => {
-    modalOpen === true
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "unset");
-  }, [modalOpen]);
-
   /**
    * 부품 분류 선택 handler -> 리스트 출력
    */
   useEffect(() => {
     if (selectClass === "all") {
-      // const allTsList: TsItem[] = [];
-      // allTsList.concat(
-      //   tsItemListB,
-      //   tsItemListD,
-      //   tsItemListE,
-      //   tsItemListH,
-      //   tsItemListS
-      // );
       setTsItemList(tsItemListAll);
     } else {
       switch (selectClass) {
@@ -91,16 +68,78 @@ const AdminMolitItemsPage: NextPage<any> = (props) => {
           return setTsItemList(tsItemListS);
       }
     }
-  }, [selectClass]);
+  }, [selectClass, reset]);
+
+  /**
+   * 검색 input handler
+   * @param e
+   */
+  const onInputSearchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  /**
+   * 검색 handler
+   * @param e
+   */
+  const onSearchFormHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!searchText) {
+      setReset(reset + 1);
+    }
+    const newList: TsItem[] = [];
+    tsItemList.forEach((tsItem: TsItem) => {
+      if (
+        tsItem.class.description.includes(searchText) ||
+        tsItem.class.label.includes(searchText) ||
+        tsItem.index.includes(searchText) ||
+        tsItem.name.includes(searchText) ||
+        tsItem.nickName?.includes(searchText) ||
+        tsItem.options?.toString().includes(searchText)
+      ) {
+        newList.push(tsItem);
+      }
+    });
+    setTsItemList(newList);
+  };
+
+  /**
+   * 국토부 작업내용 표출을 위한 생성 함수
+   * @param name
+   * @param nickName
+   * @returns
+   */
+  const genTsContent = (name: string, nickName?: any) => {
+    let tsContent = name;
+    if (nickName) tsContent += `(${nickName})`;
+    return tsContent;
+  };
+
+  /**
+   * 국토부 옵션 표출을 위한 생성 함수
+   * @param options
+   * @returns
+   */
+  const genTsOption = (options: any) => {
+    let tsOption = "";
+    if (options) {
+      for (let i = 0; i < options.length; i++) {
+        if (i === options.length - 1) {
+          tsOption += options[i];
+        } else {
+          tsOption += `${options[i]}, `;
+        }
+      }
+    } else {
+      tsOption += "-";
+    }
+    return tsOption;
+  };
 
   /*********************************************************************
    * 4. Props settings
    *********************************************************************/
-  const ARCModalProps: any = {
-    ...props,
-    setModalOpen,
-    style: { height: "500px" },
-  };
+
   /*********************************************************************
    * 5. Page configuration
    *********************************************************************/
@@ -117,29 +156,33 @@ const AdminMolitItemsPage: NextPage<any> = (props) => {
 
         <RsWrapper>
           <Wrapper padding={`50px 0px 0px`}>
-            <SearchInputWrapper
-              type="text"
-              width={`678px`}
-              padding={`0px 5px`}
-              dr={`row`}
-              borderBottom={`1px solid #000`}
-            >
-              <Wrapper width={`auto`}>
-                <SearchInput
-                  width={`632px`}
-                  padding={`0px 5px 0px 5px`}
-                  placeholder="찾고싶은 부품명을 입력하세요."
-                  type="text"
-                />
-              </Wrapper>
-              <Wrapper width={`36px`} height={`46px`}>
-                <Text fontSize={`24px`}>
-                  <IconButton type="submit" shadow={`none`}>
-                    <BsSearch />
-                  </IconButton>
-                </Text>
-              </Wrapper>
-            </SearchInputWrapper>
+            <form onSubmit={onSearchFormHandler}>
+              <SearchInputWrapper
+                type="text"
+                width={`678px`}
+                padding={`0px 5px`}
+                dr={`row`}
+                borderBottom={`1px solid #000`}
+              >
+                <Wrapper width={`auto`}>
+                  <SearchInput
+                    width={`632px`}
+                    padding={`0px 5px 0px 5px`}
+                    placeholder="찾고싶은 부품명을 입력하세요."
+                    type="text"
+                    value={searchText}
+                    onChange={onInputSearchHandler}
+                  />
+                </Wrapper>
+                <Wrapper width={`36px`} height={`46px`}>
+                  <Text fontSize={`24px`}>
+                    <IconButton type="submit" shadow={`none`}>
+                      <BsSearch />
+                    </IconButton>
+                  </Text>
+                </Wrapper>
+              </SearchInputWrapper>
+            </form>
           </Wrapper>
           <Wrapper dr={`row`} padding={`40px 0px 0px`} ju={`space-between`}>
             {/* 부품분류 */}
@@ -192,29 +235,32 @@ const AdminMolitItemsPage: NextPage<any> = (props) => {
               <TableWrapper overflow={`auto`}>
                 <Wrapper isSticky={true}>
                   <TableHead radius={`8px 8px 0px 0px`}>
-                    <TableHeadLIST width={`25%`}>분류</TableHeadLIST>
-                    <TableHeadLIST width={`15%`}>코드</TableHeadLIST>
-                    <TableHeadLIST width={`60%`}>작업내용</TableHeadLIST>
+                    <TableHeadLIST width={`20%`}>분류</TableHeadLIST>
+                    <TableHeadLIST width={`20%`}>코드</TableHeadLIST>
+                    <TableHeadLIST width={`40%`}>작업내용</TableHeadLIST>
+                    <TableHeadLIST width={`20%`}>옵션</TableHeadLIST>
                   </TableHead>
                 </Wrapper>
                 <Wrapper overflow={`auto`} height={`450px`} ju={`flex-start`}>
                   <TableBody>
-                    {tsItemList.map((item: TsItem) => (
-                      <TableRow>
-                        <TableRowLIST width={`25%`}>dd</TableRowLIST>
-                        <TableRowLIST width={`15%`}>B01</TableRowLIST>
-                        <TableRowLIST width={`60%`}>
-                          전조등(헤드램프)(좌)(우)
+                    {tsItemList.map((item: TsItem, idx: number) => (
+                      // <TableRow key={`${item.class.label}${item.index}`}>
+                      <TableRow key={idx}>
+                        <TableRowLIST width={`20%`}>
+                          {item.class.description}
+                        </TableRowLIST>
+                        <TableRowLIST width={`20%`}>
+                          {item.class.label}
+                          {item.index}
+                        </TableRowLIST>
+                        <TableRowLIST width={`40%`}>
+                          {genTsContent(item.name, item.nickName)}
+                        </TableRowLIST>
+                        <TableRowLIST width={`20%`}>
+                          {genTsOption(item.options)}
                         </TableRowLIST>
                       </TableRow>
                     ))}
-                    {/* <TableRow>
-                      <TableRowLIST width={`25%`}>차체(보디)(B)</TableRowLIST>
-                      <TableRowLIST width={`15%`}>B01</TableRowLIST>
-                      <TableRowLIST width={`60%`}>
-                        전조등(헤드램프)(좌)(우)
-                      </TableRowLIST>
-                    </TableRow> */}
                   </TableBody>
                 </Wrapper>
               </TableWrapper>
