932e6e49d278490b510d91a9798e7323228eb30b
ByunMooYoung <mk@mklc.co.kr>
2022-01-24 13:59:22 +0900

    a

--- front-end/src/models/agency.entity.ts
+++ front-end/src/models/agency.entity.ts
@@ -1,13 +1,13 @@
-import { BaseEntity } from './base.entity';
+import { BaseEntity } from "./base.entity";
 /**
  * 거래처의 연락처 정보를 저장하는 스키마
  */
 export declare class Agency extends BaseEntity {
-    name: string;
-    manager: string;
-    phoneNum: string;
-    hpNum: string;
-    address1: string;
-    address2: string;
-    memo?: string;
+  name: string;
+  manager: string;
+  phoneNum: string;
+  hpNum: string;
+  address1: string;
+  address2: string;
+  memo?: string;
 }
