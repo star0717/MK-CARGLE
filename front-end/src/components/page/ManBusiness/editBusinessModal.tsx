b0ea210a6a84f084e00df77d3321311f3c900c2a
byeol <ydb0717@gmail.com>
2022-01-25 15:29:55 +0900

    a

--- front-end/src/components/page/ManBusiness/editBusinessModal.tsx
+++ front-end/src/components/page/ManBusiness/editBusinessModal.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { NextPage } from "next";
-import { WholeWrapper } from "src/components/styles/CommonComponents";
+import {
+  CommonButton,
+  CommonButtonWrapper,
+  Text,
+  CommonSmallTitle,
+  TextArea,
+  TextInput2,
+  WholeWrapper,
+  Wrapper,
+} from "src/components/styles/CommonComponents";
 
 const EditBusinessModal: NextPage<any> = () => {
   /*********************************************************************
@@ -22,7 +31,93 @@ const EditBusinessModal: NextPage<any> = () => {
   /*********************************************************************
    * 5. Page configuration
    *********************************************************************/
-  return <WholeWrapper></WholeWrapper>;
+  return (
+    <WholeWrapper>
+      <CommonSmallTitle>거래처 상세정보</CommonSmallTitle>
+      <Wrapper>
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>*상호명</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>담당자명</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>사업자등록번호</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>전화번호</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>휴대전화번호</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>팩스번호</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>이메일</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>주소</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextInput2
+              width={`400px`}
+              margin={`0px 0px 10px 0px`}
+              type="text"
+            />
+
+            <TextInput2 width={`400px`} type="text" />
+          </Wrapper>
+        </Wrapper>
+
+        <Wrapper al={`flex-start`} margin={`0px 0px 10px`} width={`400px`}>
+          <Text>메모</Text>
+          <Wrapper width={`400px`} ju={`flex-start`}>
+            <TextArea
+              padding={`10px`}
+              width={`400px`}
+              height={`150px`}
+              placeholder="메모를 입력하세요."
+              al={`flex-start`}
+              type="text"
+            />
+          </Wrapper>
+        </Wrapper>
+        <CommonButtonWrapper kindOf={`column`}>
+          <CommonButton kindOf={`circleTheme`} type="button">
+            저장
+          </CommonButton>
+        </CommonButtonWrapper>
+      </Wrapper>
+    </WholeWrapper>
+  );
 };
 
 export default EditBusinessModal;
