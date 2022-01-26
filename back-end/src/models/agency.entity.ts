4d5caa2b6b2eb5e21633f9ea14f970ddfa13bde3
ByunMooYoung <mk@mklc.co.kr>
2022-01-26 16:25:42 +0900

    a

--- back-end/src/models/agency.entity.ts
+++ back-end/src/models/agency.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { prop } from '@typegoose/typegoose';
-import { IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 import { BaseEntity } from './base.entity';
 
 /**
@@ -17,6 +17,16 @@ export class Agency extends BaseEntity {
   })
   public name: string;
 
+  @ApiProperty({ description: '사업자등록번호' })
+  @IsOptional()
+  @IsString()
+  @prop({
+    unique: true,
+    required: [true, '사업자등록번호는 필수 항목입니다.'],
+    trim: true,
+  })
+  public comRegNum: string;
+
   @ApiProperty({ description: '담당자' })
   @IsOptional()
   @IsString()
@@ -26,6 +36,16 @@ export class Agency extends BaseEntity {
     trim: true,
   })
   public manager: string;
+
+  @ApiProperty({ description: '메일주소', default: 'example@mklc.co.kr' })
+  @IsOptional()
+  @IsEmail()
+  @prop({
+    unique: true,
+    required: [true, '메일주소는 필수 항목입니다.'],
+    trim: true,
+  })
+  public email: string;
 
   @ApiProperty({ description: '전화번호' })
   @IsOptional()
@@ -44,6 +64,25 @@ export class Agency extends BaseEntity {
     required: false,
   })
   public hpNum: string;
+
+  @ApiProperty({ description: '팩스번호' })
+  @IsOptional()
+  @IsString()
+  @prop({
+    unique: false,
+    required: [false, '팩스번호는 필수 항목입니다.'],
+  })
+  public faxNum: string;
+
+  @ApiProperty({ description: '사업장 우편 번호' })
+  @IsOptional()
+  @IsString()
+  @prop({
+    unique: false,
+    required: [true, '사업장 주소는 필수 항목입니다.'],
+    trim: true,
+  })
+  public postcode: string;
 
   @ApiProperty({ description: '사업장 주소' })
   @IsOptional()
@@ -73,5 +112,5 @@ export class Agency extends BaseEntity {
     required: false,
     trim: true,
   })
-  public memo?: string;
+  public memo: string;
 }
