c0a714e17a4649f76168c45e864246b6fefcfae3
GuHyeok <rngur1205@naver.com>
2022-02-07 16:56:24 +0900

    a

--- front-end/src/components/page/ManReservation/section/reservationList.tsx
+++ front-end/src/components/page/ManReservation/section/reservationList.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { NextPage } from "next";
-import { BodyWrapper } from "src/components/styles/LayoutComponents";
+import AddReservation from "./addReservationModal";
 import {
   CommonSubTitle,
   CommonTitle,
@@ -20,9 +20,14 @@ import {
   CheckInput,
   CheckMark,
   TableBody,
+  CloseButton,
+  CommonButton,
 } from "src/components/styles/CommonComponents";
+import Modal from "react-modal";
 import { _MainProps } from "src/configure/_props.entity";
 import { BsSearch } from "react-icons/bs";
+import { IoIosCloseCircle } from "react-icons/io";
+import EditReservation from "./editReservationModal";
 
 const ManReservationPage: NextPage<_MainProps> = (props) => {
   /*********************************************************************
@@ -32,7 +37,8 @@ const ManReservationPage: NextPage<_MainProps> = (props) => {
   /*********************************************************************
    * 2. State settings
    *********************************************************************/
-
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [modalOption, setModalOption] = useState<string>("");
   /*********************************************************************
    * 3. Handlers
    *********************************************************************/
@@ -40,7 +46,15 @@ const ManReservationPage: NextPage<_MainProps> = (props) => {
   /*********************************************************************
    * 4. Props settings
    *********************************************************************/
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
+  const ModalProps: any = {
+    ...props,
+    setModalOpen,
+    style: { height: "800px", width: "500px" },
+  };
   /*********************************************************************
    * 5. Page configuration
    *********************************************************************/
@@ -172,8 +186,71 @@ const ManReservationPage: NextPage<_MainProps> = (props) => {
                 )} */}
             </TableBody>
           </TableWrapper>
+          <Wrapper dr={`row`}>
+            <CommonButton
+              onClick={() => {
+                setModalOption("add");
+                setModalOpen(true);
+              }}
+            >
+              addModal
+            </CommonButton>
+            <CommonButton
+              onClick={() => {
+                setModalOption("edit");
+                setModalOpen(true);
+              }}
+            >
+              editModal
+            </CommonButton>
+          </Wrapper>
         </Wrapper>
       </RsWrapper>
+      <Modal
+        isOpen={modalOpen}
+        style={{
+          overlay: {
+            position: "fixed",
+            zIndex: 9999,
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            background: "rgba(71, 71, 71, 0.75)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          },
+          content: {
+            background: "white",
+            width: "500px",
+            height: "800px",
+            maxWidth: "calc(100vw - 2rem)",
+            maxHeight: "calc(100vh - 2rem)",
+            overflowY: "auto",
+            position: "relative",
+            border: "1px solid #ccc",
+            borderRadius: "0.3rem",
+            boxShadow: "0px 10px 15px rgba(61,61,61,1)",
+            inset: 0,
+          },
+        }}
+      >
+        <Wrapper fontSize={`28px`} al={`flex-end`}>
+          <CloseButton onClick={closeModal}>
+            <IoIosCloseCircle />
+          </CloseButton>
+          {/* <DaumPostcode
+              onComplete={addressHandler}
+              style={{ height: "500px" }}
+            /> */}
+        </Wrapper>
+        {modalOption === "add" ? (
+          <AddReservation {...ModalProps} />
+        ) : (
+          <EditReservation {...ModalProps} />
+        )}
+      </Modal>
     </WholeWrapper>
   );
 };
