f05980bebebca6b25bd0e44613f4482b97d33013
GuHyeok <rngur1205@naver.com>
2022-02-22 13:52:23 +0900

    a

--- front-end/src/components/page/MaintenanceBook/section/released.tsx
+++ front-end/src/components/page/MaintenanceBook/section/released.tsx
@@ -73,12 +73,20 @@ import MtSetModal from "./setModal";
 import { GoCheck } from "react-icons/go";
 import dayjs from "dayjs";
 
-const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
+const MaintenanceStored: NextPage<_pMaintenanceProps> = (props) => {
   /*********************************************************************
    * 1. Init Libs
    *********************************************************************/
   const router = useRouter();
   const dispatch = useDispatch();
+
+  // react-hook-form 사용을 위한 선언
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ criteriaMode: "all", mode: "onChange" });
 
   const workInit: MainWork[] = [
     {
@@ -108,17 +116,17 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
   const [modalOpen, setModalOpen] = useState<boolean>(false); // modal 창 여부
   const [modalOption, setModalOption] = useState<string>(""); // modal 창 옵션
   const [mtInfo, setMtInfo] = useState<Maintenance>(props.data.mtData); // 해당 정비내역 정보
+  const [vatCheck, setVatCheck] = useState<boolean>(false); // 부가세 체크여부
+  const [cellCount, setCellCount] = useState<number>(7); // 행 갯수
+  const [workList, setWorkList] = useState<MainWork[]>(workInit); // 부품 리스트
+  const [inputSum, setInputSum] = useState<number[]>([0]); // 부품 input: 계
+  const [price, setPrice] = useState<Partial<MainPrice>>(priceInit); // 가격정보
   const [partSetClass, setPartSetClass] = useState<Partial<PartsSet>[]>(
     props.data.setList.docs
   ); // 전체 세트 항목
   const [partSetData, setPartSetData] = useState<Partial<PartsSet>>(
     partSetClass[0]
   ); // 선택한 세트 데이터
-  const [vatCheck, setVatCheck] = useState<boolean>(false); // 부가세 체크여부
-  const [cellCount, setCellCount] = useState<number>(7); // 행 갯수
-  const [workList, setWorkList] = useState<MainWork[]>(workInit); // 부품 리스트
-  const [inputSum, setInputSum] = useState<number[]>([0]); // 부품 input: 계
-  const [price, setPrice] = useState<Partial<MainPrice>>(priceInit); // 가격정보
 
   /*********************************************************************
    * 3. Handlers
@@ -139,6 +147,8 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
     setCellCount(workList.length * 7);
   }, [workList]);
 
+  console.log(props.data.allParts.docs);
+
   /**
    * modal 창 닫기 기능
    */
@@ -152,7 +162,16 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
    * @param idx
    */
   const onKeyUpHandler = (e: KeyboardEvent, idx: number) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" || e.key === "ArrowRight") {
+      inputRef.current[idx + 1].focus();
+    }
+    if (e.key === "ArrowLeft") {
+      if (idx !== 0) inputRef.current[idx - 1].focus();
+    }
+    if (e.key === "ArrowUp") {
+      if (idx >= 7) inputRef.current[idx - 7].focus();
+    }
+    if (e.key === "ArrowDown") {
       inputRef.current[idx + 7].focus();
     }
   };
@@ -163,14 +182,19 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
    * @param idx
    */
   const onKeyDownhandler = (e: KeyboardEvent, idx: number) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" || e.key === "ArrowRight") {
+      if (idx === cellCount - 1) {
+        setWorkList(workList.concat(workInit));
+        setInputSum(inputSum.concat([0]));
+      }
+    }
+    if (e.key === "ArrowDown") {
       if (idx >= cellCount - 7) {
         setWorkList(workList.concat(workInit));
         setInputSum(inputSum.concat([0]));
       }
     }
   };
-
   /**
    * 정비내역 input handler
    * @param e
@@ -181,43 +205,9 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
     idx: number
   ) => {
     switch (e.target.name) {
-      case "name":
-        const partOne: Part[] = props.data.allParts.docs.filter(
-          (item: Part) =>
-            e.target.value === item.name ||
-            item.nickName.includes(e.target.value)
-        );
-        // if (e.target.value) {
-        //   setPartList(
-        //     props.data.allParts.docs.filter((item: Part) => {
-        //       for (let i = 0; i < item.nickName.length; i++) {
-        //         if (item.nickName[i].startsWith(e.target.value)) {
-        //           return item;
-        //         }
-        //       }
-        //     })
-        //   );
-        // } else {
-        //   setPartList(props.data.allParts.docs);
-        // }
-        return setWorkList(
-          workList.map((item, index) =>
-            index === idx
-              ? {
-                  ...item,
-                  name: partOne[0]?.nickName.includes(e.target.value)
-                    ? partOne[0].name
-                    : e.target.value,
-                  code: partOne[0]?.code,
-                  tsCode: partOne[0]?.tsCode || "",
-                }
-              : item
-          )
-        );
       case "price":
       case "quantity":
       case "wage":
-        e.target.value = e.target.value.replaceAll(",", "");
         if (e.target.value === "" || !basicRegEx.NUM.test(e.target.value)) {
           return setWorkList(
             workList.map((item, index) =>
@@ -228,30 +218,34 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
           return setWorkList(
             workList.map((item, index) =>
               index === idx
-                ? { ...item, [e.target.name]: Number(e.target.value) }
+                ? { ...item, [e.target.name]: parseInt(e.target.value) }
                 : item
             )
           );
         }
+
       default:
+        if (e.target.name === "name") {
+          const partOne: Part[] = props.data.allParts.docs.filter(
+            (item: Part) => e.target.value === item.name
+          );
+          return setWorkList(
+            workList.map((item, index) =>
+              index === idx
+                ? {
+                    ...item,
+                    name: e.target.value,
+                    tsCode: partOne[0]?.tsCode || "",
+                  }
+                : item
+            )
+          );
+        }
         return setWorkList(
           workList.map((item, index) =>
             index === idx ? { ...item, [e.target.name]: e.target.value } : item
           )
         );
-    }
-  };
-
-  console.log(workList[0].code);
-
-  /**
-   * 열 삭제 handler
-   * @param idx
-   */
-  const onDeleteRowHandler = (idx: number) => {
-    if (workList.length > 1) {
-      setInputSum(inputSum.filter((data, index) => idx !== index));
-      setWorkList(workList.filter((data, index) => idx !== index));
     }
   };
 
@@ -284,12 +278,13 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
       ...price,
       partsSum: partsSum,
       wageSum: wageSum,
-      sum: Number(sum2.toString().split(".")[0]),
-      vat: Number(vat.toString().split(".")[0]),
-      total: Number((sum2 + vat).toString().split(".")[0]),
+      sum: parseInt(sum2.toString().split(".")[0]),
+      vat: parseInt(vat.toString().split(".")[0]),
+      total: sum2 + vat,
     });
   }, [workList, vatCheck]);
 
+  // console.log("part", props.data.allParts.docs);
   /*********************************************************************
    * 4. Props settings
    *********************************************************************/
@@ -302,8 +297,6 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
     setPartSetData,
     workList,
     setWorkList,
-    inputSum,
-    setInputSum,
   };
 
   /*********************************************************************
@@ -382,7 +375,9 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                   type="button"
                   shadow={`none`}
                   onClick={() => {
-                    router.push(`${UseLink.MAINTENANCE_BOOK}?step=c`);
+                    router.push(
+                      `${UseLink.MAINTENANCE_BOOK}?step=${MainStatus.STORED}`
+                    );
                   }}
                 >
                   <AiFillCloseCircle />
@@ -502,6 +497,19 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                   readOnly
                 />
               </Wrapper>
+              <Wrapper dr={`row`} ju={`space-between`} padding={`10px 0px`}>
+                <SmallButton width={`48%`} kindOf={`default`}>
+                  정비요청사항
+                </SmallButton>
+                <SmallButton width={`48%`} kindOf={`default`}>
+                  차량정보공유
+                </SmallButton>
+              </Wrapper>
+              <Wrapper>
+                <SmallButton width={`100%`} kindOf={`default`}>
+                  정비사진확인
+                </SmallButton>
+              </Wrapper>
             </Wrapper>
           </Wrapper>
 
@@ -525,6 +533,24 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                   }}
                 >
                   서류발급
+                </SmallButton>
+                <SmallButton
+                  type="button"
+                  kindOf={`default`}
+                  onClick={() => {
+                    console.log("서류");
+                  }}
+                >
+                  국토부
+                </SmallButton>
+                <SmallButton
+                  type="button"
+                  kindOf={`default`}
+                  onClick={() => {
+                    console.log("서류");
+                  }}
+                >
+                  결재정보
                 </SmallButton>
               </Wrapper>
             </Wrapper>
@@ -550,9 +576,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                     width={`150px`}
                     type="text"
                     value={
-                      mtInfo.dates.startMa
-                        ? dayjs(mtInfo.dates.startMa).format("YYYY-MM-DD")
-                        : "-"
+                      dayjs(mtInfo.dates.startMa).format("YYYY-MM-DD") || "-"
                     }
                     disabled
                   />
@@ -567,9 +591,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                     width={`150px`}
                     type="text"
                     value={
-                      mtInfo.dates.endMa
-                        ? dayjs(mtInfo.dates.endMa).format("YYYY-MM-DD")
-                        : "-"
+                      dayjs(mtInfo.dates.endMa).format("YYYY-MM-DD") || "-"
                     }
                     disabled
                   />
@@ -586,9 +608,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                     width={`150px`}
                     type="text"
                     value={
-                      mtInfo.dates.released
-                        ? dayjs(mtInfo.dates.released).format("YYYY-MM-DD")
-                        : "-"
+                      dayjs(mtInfo.dates.released).format("YYYY-MM-DD") || "-"
                     }
                     disabled
                   />
@@ -637,7 +657,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                     type="text"
                     width={`150px`}
                     placeholder={`보험사명 입력란`}
-                    readOnly
+                    disabled
                   />
                 </Wrapper>
                 <Wrapper
@@ -649,7 +669,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                     type="text"
                     width={`240px`}
                     placeholder={`보험번호 입력란`}
-                    readOnly
+                    disabled
                   />
                 </Wrapper>
                 <Wrapper dr={`row`} ju={`flex-end`}>
@@ -719,11 +739,10 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
             </Wrapper>
             <TableWrapper minHeight={`auto`}>
               <TableHead>
-                <TableHeadLIST width={`3%`}></TableHeadLIST>
-                <TableHeadLIST width={`14%`}>작업내용</TableHeadLIST>
-                <TableHeadLIST width={`14%`}>국토부</TableHeadLIST>
+                <TableHeadLIST width={`15%`}>작업내용</TableHeadLIST>
+                <TableHeadLIST width={`15%`}>국토부</TableHeadLIST>
                 <TableHeadLIST width={`14%`}>구분</TableHeadLIST>
-                <TableHeadLIST width={`14%`}>단가</TableHeadLIST>
+                <TableHeadLIST width={`15%`}>단가</TableHeadLIST>
                 <TableHeadLIST width={`14%`}>수량</TableHeadLIST>
                 <TableHeadLIST width={`14%`}>계</TableHeadLIST>
                 <TableHeadLIST width={`8%`}>기술료</TableHeadLIST>
@@ -732,20 +751,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                 {workList.map((data, idx) => {
                   return (
                     <TableRow key={idx} kindOf={`noHover`}>
-                      <TableRowLIST width={`3%`}>
-                        <IconButton
-                          type="button"
-                          shadow={`none`}
-                          bgColor={`inherit`}
-                          margin={`0px`}
-                          onClick={() => {
-                            onDeleteRowHandler(idx);
-                          }}
-                        >
-                          <AiFillCloseCircle />
-                        </IconButton>
-                      </TableRowLIST>
-                      <TableRowLIST width={`14%`}>
+                      <TableRowLIST width={`15%`}>
                         <TextInput2
                           type="text"
                           ref={(elem: HTMLInputElement) =>
@@ -760,14 +766,14 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                           }
                           value={data.name}
                           name="name"
-                          list="workList"
+                          list="worklist"
                           onChange={(
                             e: React.ChangeEvent<HTMLInputElement>
                           ) => {
                             onChangeInputArr(e, idx);
                           }}
                         />
-                        <datalist id="workList">
+                        <datalist id="worklist">
                           {props.data.allParts.docs.map(
                             (item: Part, idx: number) => {
                               return <option key={idx} value={item.name} />;
@@ -775,7 +781,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                           )}
                         </datalist>
                       </TableRowLIST>
-                      <TableRowLIST width={`14%`}>
+                      <TableRowLIST width={`15%`}>
                         <TextInput2
                           type="text"
                           ref={(elem: HTMLInputElement) =>
@@ -822,7 +828,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                           })}
                         </Combo>
                       </TableRowLIST>
-                      <TableRowLIST width={`14%`}>
+                      <TableRowLIST width={`15%`}>
                         <TextInput2
                           type="text"
                           ref={(elem: HTMLInputElement) =>
@@ -837,6 +843,7 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
                           }
                           value={data.price.toLocaleString()}
                           name="price"
+                          ㅣㄴ
                           onChange={(
                             e: React.ChangeEvent<HTMLInputElement>
                           ) => {
@@ -912,57 +919,34 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
               </TableBody>
             </TableWrapper>
             <Wrapper dr={`row`} ju={`flex-end`}>
-              <Text>부품계 : {price.partsSum.toLocaleString()}</Text>
+              <Text>부품계 : 0 </Text>
               <Text fontSize={`12px`} fontWeight={`800`} margin={`0px 10px`}>
                 |
               </Text>
-              <Text>기술료계 : {price.wageSum.toLocaleString()}</Text>
+              <Text>기술료계 : 0 </Text>
               <Text fontSize={`12px`} fontWeight={`800`} margin={`0px 10px`}>
                 |
               </Text>
-              <Text>합계 : {price.sum.toLocaleString()}</Text>
+              <Text>합계 : 0 </Text>
               <Text fontSize={`12px`} fontWeight={`800`} margin={`0px 10px`}>
                 |
               </Text>
-              <Text>부가세 : {price.vat.toLocaleString()}</Text>
+              <Text>부가세 : 0</Text>
               <Text fontSize={`12px`} fontWeight={`800`} margin={`0px 10px`}>
                 |
               </Text>
-              <Text fontSize={`24px`}>총계</Text>
-              <Text
-                fontSize={`24px`}
-                fontWeight={`800`}
-                color={`#314FA5`}
-                margin={`0px 10px`}
-              >
-                {price.total.toLocaleString()}
+              <Text fontSize={`24px`}>
+                총계 <ColorSpan color={`#314FA5`}>0</ColorSpan>
               </Text>
             </Wrapper>
             <Wrapper dr={`row`} ju={`space-between`}>
               <SmallButton
                 form="carInfoForm"
                 type="submit"
-                kindOf={`ghost`}
-                disabled
-                width={`100%`}
-              >
-                이전단계
-              </SmallButton>
-              <SmallButton
-                form="carInfoForm"
-                type="submit"
                 kindOf={`default`}
                 width={`100%`}
               >
-                저장
-              </SmallButton>
-              <SmallButton
-                form="carInfoForm"
-                type="submit"
-                kindOf={`default`}
-                width={`100%`}
-              >
-                다음단계
+                정비내역 수정
               </SmallButton>
             </Wrapper>
           </Wrapper>
@@ -1013,4 +997,4 @@ const MaintenanceReleased: NextPage<_pMaintenanceProps> = (props) => {
   );
 };
 
-export default MaintenanceReleased;
+export default MaintenanceStored;
