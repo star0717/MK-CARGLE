1f3656d8ba51ebefafa7d678bbec13df1459fee5
byeol <ydb0717@gmail.com>
2022-03-17 22:31:34 +0900

    a

--- front-end/src/components/page/SignUp/section/fileUpload.tsx
+++ front-end/src/components/page/SignUp/section/fileUpload.tsx
@@ -34,6 +34,7 @@ import { _pFileUploadProps } from "../../../../configure/_pProps.entity";
 import { AiOutlineFileText, AiOutlineUser } from "react-icons/ai";
 import { GoCheck } from "react-icons/go";
 import { MdOutlineBusinessCenter, MdOutlineUploadFile } from "react-icons/md";
+import { BodyWrapper } from "src/components/styles/LayoutComponents";
 
 /**
  * 파일 데이터 초기화
@@ -136,197 +137,210 @@ const FileUpload: NextPage<_pFileUploadProps> = (props) => {
   const { width, height, ref } = useResizeDetector();
 
   return (
-    <WholeWrapper ref={ref}>
-      <RsWrapper>
-        <CommonTitleWrapper>
-          <CommonTitle>회원가입</CommonTitle>
-          <CommonSubTitle>가입 승인을 위해 서류를 제출해주세요</CommonSubTitle>
-        </CommonTitleWrapper>
-        <JoinStepBarWrapper>
-          <Wrapper width={`auto`}>
-            <JoinStepBar
-              kindOf={props.stepNumber === 2 ? `progress` : `complete`}
-            >
-              {props.stepNumber === 2 ? <AiOutlineFileText /> : <GoCheck />}
-            </JoinStepBar>
-            <Text height={`0px`} padding={`10px 0px 0px`}>
-              약관동의
-            </Text>
-          </Wrapper>
-          <JoinStepBar
-            kindOf={props.stepNumber > 2 ? `line` : `line2`}
-          ></JoinStepBar>
-          <Wrapper width={`auto`}>
-            <JoinStepBar
-              kindOf={
-                props.stepNumber < 3
-                  ? `before`
-                  : props.stepNumber === 3
-                  ? `progress`
-                  : `complete`
-              }
-            >
-              {props.stepNumber > 3 ? <GoCheck /> : <AiOutlineUser />}
-            </JoinStepBar>
-            <Text height={`0px`} padding={`10px 0px 0px`}>
-              계정정보
-            </Text>
-          </Wrapper>
-          <JoinStepBar
-            kindOf={props.stepNumber > 3 ? `line` : `line2`}
-          ></JoinStepBar>
-          <Wrapper width={`auto`}>
-            <JoinStepBar
-              kindOf={
-                props.stepNumber < 4
-                  ? `before`
-                  : props.stepNumber === 4
-                  ? `progress`
-                  : `complete`
-              }
-            >
-              {props.stepNumber > 4 ? <GoCheck /> : <MdOutlineBusinessCenter />}
-            </JoinStepBar>
-            <Text height={`0px`} padding={`10px 0px 0px`}>
-              사업자정보
-            </Text>
-          </Wrapper>
-          <JoinStepBar
-            kindOf={props.stepNumber > 4 ? `line` : `line2`}
-          ></JoinStepBar>
-          <Wrapper width={`auto`}>
-            <JoinStepBar
-              kindOf={props.stepNumber === 5 ? `progress` : `before`}
-            >
-              <MdOutlineUploadFile />
-            </JoinStepBar>
-            <Text height={`0px`} padding={`10px 0px 0px`}>
-              서류제출
-            </Text>
-          </Wrapper>
-        </JoinStepBarWrapper>
-        <form onSubmit={onFileUploadHandler}>
-          <Wrapper
-            width={`auto`}
-            padding={`50px`}
-            border={`1px solid #ccc`}
-            radius={`5px`}
-          >
+    <BodyWrapper ref={ref}>
+      <WholeWrapper>
+        <RsWrapper>
+          <CommonTitleWrapper>
+            <CommonTitle>회원가입</CommonTitle>
+            <CommonSubTitle>
+              가입 승인을 위해 서류를 제출해주세요
+            </CommonSubTitle>
+          </CommonTitleWrapper>
+          {router.query.page[0] !== "main" && (
+            <JoinStepBarWrapper>
+              <Wrapper width={`auto`}>
+                <JoinStepBar
+                  kindOf={props.stepNumber === 2 ? `progress` : `complete`}
+                >
+                  {props.stepNumber === 2 ? <AiOutlineFileText /> : <GoCheck />}
+                </JoinStepBar>
+                <Text height={`0px`} padding={`10px 0px 0px`}>
+                  약관동의
+                </Text>
+              </Wrapper>
+              <JoinStepBar
+                kindOf={props.stepNumber > 2 ? `line` : `line2`}
+              ></JoinStepBar>
+              <Wrapper width={`auto`}>
+                <JoinStepBar
+                  kindOf={
+                    props.stepNumber < 3
+                      ? `before`
+                      : props.stepNumber === 3
+                      ? `progress`
+                      : `complete`
+                  }
+                >
+                  {props.stepNumber > 3 ? <GoCheck /> : <AiOutlineUser />}
+                </JoinStepBar>
+                <Text height={`0px`} padding={`10px 0px 0px`}>
+                  계정정보
+                </Text>
+              </Wrapper>
+              <JoinStepBar
+                kindOf={props.stepNumber > 3 ? `line` : `line2`}
+              ></JoinStepBar>
+              <Wrapper width={`auto`}>
+                <JoinStepBar
+                  kindOf={
+                    props.stepNumber < 4
+                      ? `before`
+                      : props.stepNumber === 4
+                      ? `progress`
+                      : `complete`
+                  }
+                >
+                  {props.stepNumber > 4 ? (
+                    <GoCheck />
+                  ) : (
+                    <MdOutlineBusinessCenter />
+                  )}
+                </JoinStepBar>
+                <Text height={`0px`} padding={`10px 0px 0px`}>
+                  사업자정보
+                </Text>
+              </Wrapper>
+              <JoinStepBar
+                kindOf={props.stepNumber > 4 ? `line` : `line2`}
+              ></JoinStepBar>
+              <Wrapper width={`auto`}>
+                <JoinStepBar
+                  kindOf={props.stepNumber === 5 ? `progress` : `before`}
+                >
+                  <MdOutlineUploadFile />
+                </JoinStepBar>
+                <Text height={`0px`} padding={`10px 0px 0px`}>
+                  서류제출
+                </Text>
+              </Wrapper>
+            </JoinStepBarWrapper>
+          )}
+          <form onSubmit={onFileUploadHandler}>
             <Wrapper
-              ju={`flex-start`}
-              al={`flex-start`}
               width={`auto`}
-              margin={`0px 0px 10px`}
+              padding={`50px`}
+              border={`1px solid #ccc`}
+              radius={`5px`}
             >
-              <Text margin={`0px 0px 10px`}>사업자등록증</Text>
               <Wrapper
-                dr={`row`}
                 ju={`flex-start`}
-                margin={`0px 0px 10px`}
+                al={`flex-start`}
                 width={`auto`}
+                margin={`0px 0px 10px`}
               >
-                <TextInput2
-                  width={`300px`}
-                  type="text"
-                  placeholder="jpg, png, pdf 형식"
-                  value={fileName.comFile}
-                  required
-                  readOnly
-                />
-                <LabelButton
-                  kindOf={`default`}
-                  margin={`0px 0px 0px 20px`}
-                  htmlFor="comFile"
+                <Text margin={`0px 0px 10px`}>사업자등록증</Text>
+                <Wrapper
+                  dr={`row`}
+                  ju={`flex-start`}
+                  margin={`0px 0px 10px`}
+                  width={`auto`}
                 >
-                  파일선택
-                </LabelButton>
-                <TextInput2
-                  style={{ display: "none" }}
-                  type="file"
-                  id="comFile"
-                  name="comFile"
-                  key={file.comFile}
-                  onChange={onFileSelectHandler}
-                  accept=".jpg, .png, .pdf"
-                />
-              </Wrapper>
-            </Wrapper>
-            <Wrapper ju={`flex-start`} al={`flex-start`} width={`auto`}>
-              <Text margin={`0px 0px 10px`}>정비업등록증</Text>
-              <Wrapper dr={`row`} ju={`flex-start`} al={`flex-start`}>
-                <TextInput2
-                  width={`300px`}
-                  type="text"
-                  placeholder="jpg, png, pdf 형식"
-                  value={fileName.manFile}
-                  required
-                  readOnly
-                />
-                <LabelButton
-                  kindOf={`default`}
-                  margin={`0px 0px 0px 20px`}
-                  htmlFor="manFile"
-                >
-                  파일선택
-                </LabelButton>
-                <TextInput2
-                  style={{ display: "none" }}
-                  type="file"
-                  id="manFile"
-                  name="manFile"
-                  key={file.manFile}
-                  onChange={onFileSelectHandler}
-                  accept=".jpg, .png, .pdf"
-                />
-              </Wrapper>
-            </Wrapper>
-          </Wrapper>
-          <Wrapper padding={`50px 0px 50px 0px`}>
-            <CommonButton
-              type="button"
-              margin={`0px 0px 10px 0px`}
-              kindOf={`white`}
-              onClick={onSignOutHandler}
-            >
-              다음에하기
-            </CommonButton>
-            <CommonButton type="submit" margin={`10px 0px 0px 0px`}>
-              제출하기
-            </CommonButton>
-          </Wrapper>
-          <Wrapper padding={`0px 0px 0px`}>
-            <Wrapper
-              borderTop={`1px solid #c4c4c4`}
-              al={`flex-start`}
-              width={`500px`}
-            >
-              <Wrapper al={`flex-end`} padding={`30px 15px 10px 0px`}></Wrapper>
-              <Wrapper padding={`0px 10px 30px`}>
-                <Wrapper dr={`row`} ju={`flex-start`} margin={`0px 0px 10px`}>
-                  <Text padding={`3px 15px 0px 0px`}>
-                    <BsFillQuestionCircleFill />
-                  </Text>
-                  <Text>아직 서류가 준비되지 않으셨나요?</Text>
+                  <TextInput2
+                    width={`300px`}
+                    type="text"
+                    placeholder="jpg, png, pdf 형식"
+                    value={fileName.comFile}
+                    required
+                    readOnly
+                  />
+                  <LabelButton
+                    kindOf={`default`}
+                    margin={`0px 0px 0px 20px`}
+                    htmlFor="comFile"
+                  >
+                    파일선택
+                  </LabelButton>
+                  <TextInput2
+                    style={{ display: "none" }}
+                    type="file"
+                    id="comFile"
+                    name="comFile"
+                    key={file.comFile}
+                    onChange={onFileSelectHandler}
+                    accept=".jpg, .png, .pdf"
+                  />
                 </Wrapper>
+              </Wrapper>
+              <Wrapper ju={`flex-start`} al={`flex-start`} width={`auto`}>
+                <Text margin={`0px 0px 10px`}>정비업등록증</Text>
                 <Wrapper dr={`row`} ju={`flex-start`} al={`flex-start`}>
-                  <Text padding={`3px 15px 0px 0px`}>
-                    <BsFillLightbulbFill />
-                  </Text>
-                  <Text textAlign={`start`}>
-                    서류가 준비되지 않으셨더라도 회원가입 시 입력한 계정정보로
-                    <br />
-                    로그인하면 이어서 진행이 가능해요.
-                    <br />
-                    <br />
-                    서류가 제출되면 최종 가입 심사가 시작됩니다!
-                  </Text>
+                  <TextInput2
+                    width={`300px`}
+                    type="text"
+                    placeholder="jpg, png, pdf 형식"
+                    value={fileName.manFile}
+                    required
+                    readOnly
+                  />
+                  <LabelButton
+                    kindOf={`default`}
+                    margin={`0px 0px 0px 20px`}
+                    htmlFor="manFile"
+                  >
+                    파일선택
+                  </LabelButton>
+                  <TextInput2
+                    style={{ display: "none" }}
+                    type="file"
+                    id="manFile"
+                    name="manFile"
+                    key={file.manFile}
+                    onChange={onFileSelectHandler}
+                    accept=".jpg, .png, .pdf"
+                  />
                 </Wrapper>
               </Wrapper>
             </Wrapper>
-          </Wrapper>
-        </form>
-      </RsWrapper>
-    </WholeWrapper>
+            <Wrapper padding={`50px 0px 50px 0px`}>
+              <CommonButton
+                type="button"
+                margin={`0px 0px 10px 0px`}
+                kindOf={`white`}
+                onClick={onSignOutHandler}
+              >
+                다음에하기
+              </CommonButton>
+              <CommonButton type="submit" margin={`10px 0px 0px 0px`}>
+                제출하기
+              </CommonButton>
+            </Wrapper>
+            <Wrapper padding={`0px 0px 0px`}>
+              <Wrapper
+                borderTop={`1px solid #c4c4c4`}
+                al={`flex-start`}
+                width={`500px`}
+              >
+                <Wrapper
+                  al={`flex-end`}
+                  padding={`30px 15px 10px 0px`}
+                ></Wrapper>
+                <Wrapper padding={`0px 10px 30px`}>
+                  <Wrapper dr={`row`} ju={`flex-start`} margin={`0px 0px 10px`}>
+                    <Text padding={`3px 15px 0px 0px`}>
+                      <BsFillQuestionCircleFill />
+                    </Text>
+                    <Text>아직 서류가 준비되지 않으셨나요?</Text>
+                  </Wrapper>
+                  <Wrapper dr={`row`} ju={`flex-start`} al={`flex-start`}>
+                    <Text padding={`3px 15px 0px 0px`}>
+                      <BsFillLightbulbFill />
+                    </Text>
+                    <Text textAlign={`start`}>
+                      서류가 준비되지 않으셨더라도 회원가입 시 입력한 계정정보로
+                      <br />
+                      로그인하면 이어서 진행이 가능해요.
+                      <br />
+                      <br />
+                      서류가 제출되면 최종 가입 심사가 시작됩니다!
+                    </Text>
+                  </Wrapper>
+                </Wrapper>
+              </Wrapper>
+            </Wrapper>
+          </form>
+        </RsWrapper>
+      </WholeWrapper>
+    </BodyWrapper>
   );
 };
 
