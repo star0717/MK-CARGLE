372f3e7481a1146e64dc9a2bcc3d055773a02bda
ByunMooYoung <mk@mklc.co.kr>
2022-03-29 19:29:18 +0900

    a

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,47 +1,10 @@
-/** @type {import('next').NextConfig} */
-// const withTM = require("next-transpile-modules")([
-//   "@mui/material",
-//   "@mui/system",
-//   "@mui/icons-material",
-// ]);
-
-// module.exports = withTM({
-//   reactStrictMode: true,
-//   webpack: (config) => {
-//     config.resolve.alias = {
-//       ...config.resolve.alias,
-//       "@mui/styled-engine": "@mui/styled-engine-sc",
-//     };
-//     return config;
-//   },
-//   async rewrites() {
-//     if (process.env.NODE_ENV !== "production") {
-//       return [
-//         {
-//           source: process.env.SOURCE_PATH,
-//           destination:
-//             process.env.DESTINATION_URL +
-//             process.env.DESTINATION_PORT +
-//             process.env.DESTINATION_PATH,
-//         },
-//       ];
-//     } else {
-//       return [
-//         {
-//           source: process.env.SOURCE_PATH,
-//           destination:
-//             process.env.DESTINATION_URL +
-//             process.env.DESTINATION_PORT +
-//             process.env.DESTINATION_PATH,
-//         },
-//       ];
-//     }
-//   },
-// });
-
 const node_env = process.env.NODE_ENV;
 
 module.exports = {
+  experimental: {
+    outputStandalone: true,
+  },
+
   async rewrites() {
     if (node_env !== "production") {
       return [
