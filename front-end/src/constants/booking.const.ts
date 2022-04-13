35dfd9781d632b6be9ffff711ac7ecdaef91b731
ByunMooYoung <hbc3869@gmail.com>
2022-04-13 18:47:15 +0900

    a

--- front-end/src/constants/booking.const.ts
+++ front-end/src/constants/booking.const.ts
@@ -1,6 +1,16 @@
+import { BookingFindOptions } from 'src/models/booking.entity';
+
 export enum BookingState {
   NEW = 'new',
   APPROVAL = 'approval',
   REJECT = 'reject',
   MAINTENANCE = 'maintenance',
 }
+
+/********** 검색 옵션 쿼리 생성 ******************/
+export const genBookingOptionQuery = (data: BookingFindOptions) => {
+  let query = '&';
+  if (data?.regNumber) query += 'regNumber=' + data.regNumber;
+  if (data?.phoneNumber) query += 'phoneNumber=' + data.phoneNumber;
+  return query;
+};
